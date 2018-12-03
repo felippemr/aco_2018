486774d0d0c2f5dcd5781c3393489d492e7723ed
Felippe Raposo <raposo.felippe@gmail.com>
2018-12-03 11:12:16 -0200

    Comment puzzle 2 answer

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,26 +1,46 @@
 use std::fs;
-use std::collections::HashSet;
 
+
+// puzzle 1
 fn main() {
     let data = fs::read_to_string("assets/input.txt").expect("Unable to read file!");
-    let mut frequencies = HashSet::new();
-
     let mut acc: i32 = 0;
-    loop {
-        let data = data.split("\n");
-        for value in data {
-            match value.parse::<i32>() {
-                Ok(e) => {
-                    acc += e;
-                    if frequencies.contains(&acc) {
-                        println!("{}", acc);
-                        return ();
-                    } else {
-                        frequencies.insert(acc);
-                    }
-                }
-                _ => {}
-            }
+    let data = data.split("\n");
+    
+    for value in data {
+        match value.parse::<i32>() {
+            Ok(e) => { acc += e }
+            _ => {}
         }
     }
+    print!("{}", acc);
 }
+
+
+// puzzle 2
+//use std::fs;
+//use std::collections::HashSet;
+
+//fn main() {
+//    let data = fs::read_to_string("assets/input.txt").expect("Unable to read file!");
+//    let mut frequencies = HashSet::new();
+//
+//    let mut acc: i32 = 0;
+//    loop {
+//        let data = data.split("\n");
+//        for value in data {
+//            match value.parse::<i32>() {
+//                Ok(e) => {
+//                    acc += e;
+//                    if frequencies.contains(&acc) {
+//                        println!("{}", acc);
+//                        return ();
+//                    } else {
+//                        frequencies.insert(acc);
+//                    }
+//                }
+//                _ => {}
+//            }
+//        }
+//    }
+//}
