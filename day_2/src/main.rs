78d1c45a5666ca78def0ae5ff263bfa6d5e4a73d
Felippe Raposo <raposo.felippe@gmail.com>
2018-12-07 10:12:29 -0200

    day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,44 +1,67 @@
+//use std::fs::File;
+//use std::io::{ BufRead, BufReader };
+//use std::collections::HashMap;
+//
+//
+//fn main() {
+//    let file = File::open("assets/input.txt").expect("Unable to read file!");
+//    let buffer = BufReader::new(file);
+//
+//    let mut count_2 = 0;
+//    let mut count_3 = 0;
+//
+//    for line in buffer.lines() {
+//        let line = line.expect("Unable to read line");
+//        let (has_2, has_3) = count_letters(&line);
+//        if has_2 {
+//            count_2 += 1;
+//        }
+//        if has_3 {
+//            count_3 += 1;
+//        }
+//    }
+//    println!("{}*{}={}", count_2, count_3, count_2 * count_3);
+//
+//}
+//
+//fn count_letters(word: &str) -> (bool, bool) {
+//    let mut counter = HashMap::new();
+//    for letter in word.chars() {
+//        let count = counter.entry(letter).or_insert(0);
+//        *count +=1;
+//    }
+//    let mut has_two = false;
+//    let mut has_three = false;
+//    for (_, value) in counter.iter() {
+//        if *value == 2 {
+//            has_two = true
+//        }
+//        if *value == 3 {
+//            has_three = true
+//        }
+//    }
+//    (has_two, has_three)
+//}
+
 use std::fs::File;
 use std::io::{ BufRead, BufReader };
-use std::collections::HashMap;
+use std::collections::HashSet;
 
-
+// puzzle 2
 fn main() {
-    let file = File::open("assets/input.txt").expect("Unable to read file!");
-    let buffer = BufReader::new(file);
-
-    let mut count_2 = 0;
-    let mut count_3 = 0;
-
-    for line in buffer.lines() {
-        let line = line.expect("Unable to read line");
-        let (has_2, has_3) = count_letters(&line);
-        if has_2 {
-            count_2 += 1;
-        }
-        if has_3 {
-            count_3 += 1;
-        }
-    }
-    println!("{}*{}={}", count_2, count_3, count_2 * count_3);
-
-}
-
-fn count_letters(word: &str) -> (bool, bool) {
-    let mut counter = HashMap::new();
-    for letter in word.chars() {
-        let count = counter.entry(letter).or_insert(0);
-        *count +=1;
-    }
-    let mut has_two = false;
-    let mut has_three = false;
-    for (_, value) in counter.iter() {
-        if *value == 2 {
-            has_two = true
-        }
-        if *value == 3 {
-            has_three = true
+    for index in 0..26 {
+        let mut words = HashSet::new();
+        let file = File::open("assets/input.txt").expect("Unable to read file!");
+        let buffer = BufReader::new(file);
+        for line in buffer.lines() {
+            let line = line.expect("Unable to read line");
+            let word = format!("{}{}", &line[0..index], &line[index+1..]);
+            if words.contains(&word) {
+                println!("{}", word);
+                return;
+            } else {
+                words.insert(word);
+            }
         }
     }
-    (has_two, has_three)
 }
